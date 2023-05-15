d0d7a7dfe0cc82a1d88440766c78910b9c4502ec
LeoKadaji <carlosotoline@gmail.com>
2023-05-14 22:09:18 -0300

    first commit

---
+++ src/Aeronave.java
@@ -0,0 +1,19 @@
+public class Aeronave {
+
+    protected String Modelo;
+    protected String NumeroSerie;
+
+    public String getModelo() {
+        return Modelo;
+    }
+    public void setModelo(String modelo) {
+        this.Modelo = modelo;
+    }
+    public String getNumeroSerie() {
+        return NumeroSerie;
+    }
+    public void setNumeroSerie(String numeroSerie) {
+        this.NumeroSerie = numeroSerie;
+    }
+    
+}
---
+++ src/Pessoa.java
@@ -0,0 +1,19 @@
+public class Pessoa {
+    
+    protected String Nome;
+    protected String Cpf;
+
+    public String getNome() {
+        return Nome;
+    }
+    public void setNome(String nome) {
+        this.Nome = nome;
+    }
+    public String getCpf() {
+        return Cpf;
+    }
+    public void setCpf(String cpf) {
+        this.Cpf = cpf;
+    }
+
+}
